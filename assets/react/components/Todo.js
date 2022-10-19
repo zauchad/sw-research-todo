015b686db67872dddaad71156e13d7e0efc40cc3
Daniel Zaucha <zauchad@gmail.com>
2022-10-19 07:44:39 +0200

    React drag and drop setup

--- assets/react/components/Todo.js
+++ assets/react/components/Todo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Todo = ({name}) => (
-    <div className="card mb-3 w-75 m-auto">
+const Todo = ({innerRef, name}) => (
+    <div ref={innerRef} className="card mb-3 w-75 m-auto">
         <div className="card-body d-flex justify-content-between align-items-center">
             <h5 className="card-title d-inline-block me-2">{name}</h5>
             <a href="#" className="d-inline-block btn btn-primary">Update</a>
